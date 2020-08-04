e39ca55c91ae09492fc871506c5e4caf4ae76e9d
mylhow <dgascon@student.42lyon.fr>
2020-08-04 16:48:31 +0200

    Fix name

--- srcs/etc/nginx/sites-available/index.sh
+++ srcs/etc/nginx/sites-available/index.sh
@@ -1,5 +1,5 @@
-if [ "${AUTOINDEX}" = "off" ]
+if [ "${AUTOINDEX}" = "off" ] | [ "${AUTOINDEX}" = "OFF" ]
 then
-  sed -i "s/autoindex on/autoindex off/" /etc/nginx/sites-available/default.conf;
-  sed -i "s/#index index.php welcome.html;/index index.php welcome.html;/" /etc/nginx/sites-available/default.conf;
+  sed -i "s/autoindex on/autoindex off/" /etc/nginx/sites-available/default;
+  sed -i "s/#index index.php welcome.html;/index index.php welcome.html;/" /etc/nginx/sites-available/default;
 fi
