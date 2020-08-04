84457a22947feb4f626deecbda5ce5526d9360d3
mylhow <dgascon@student.42lyon.fr>
2020-08-04 16:58:18 +0200

    Fix 'or'

--- srcs/etc/nginx/sites-available/index.sh
+++ srcs/etc/nginx/sites-available/index.sh
@@ -1,4 +1,4 @@
-if [ "${AUTOINDEX}" = "off" ] | [ "${AUTOINDEX}" = "OFF" ]
+if [ "${AUTOINDEX}" = "off" ] || [ "${AUTOINDEX}" = "OFF" ]
 then
   sed -i "s/autoindex on/autoindex off/" /etc/nginx/sites-available/default;
   sed -i "s/#index index.php welcome.html;/index index.php welcome.html;/" /etc/nginx/sites-available/default;
